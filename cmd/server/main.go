cc58ff9fb54149495f979cc4ff92e45e3fa83779
tibin-peter <tibinpeter0@gmail.com>
2026-05-26 19:28:59 +0530

    updated the url in main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -44,11 +44,11 @@ func main() {
 
 	//for frontend connect
 	app.Use(cors.New(cors.Config{
-		AllowOrigins:     "https://crypto-project-frontend-gold.vercel.app/,http://localhost:5173",
-		AllowMethods:     "GET,POST,PUT,DELETE,PATCH,OPTIONS",
-		AllowHeaders:     "Origin, Content-Type, Accept,Authorization",
-		AllowCredentials: true,
-	}))
+    AllowOrigins: "http://localhost:5173, https://crypto-project-frontend-gold.vercel.app, https://crypto-project-frontend-git-main-tibin-peters-projects.vercel.app, https://crypto-project-frontend-ckw39wv16-tibin-peters-projects.vercel.app",
+    AllowMethods: "GET,POST,PUT,DELETE,PATCH,OPTIONS",
+    AllowHeaders: "Origin, Content-Type, Accept, Authorization",
+    AllowCredentials: true,
+}))
 
 	//setup routes
 	centralroutes.SetUp(app, db, rdb, cfg.JWTSecret, cfg.RazorpayKey, cfg.RazorpaySecret)
